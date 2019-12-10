7cd31ce247ba3251204ef3607e3007fe1ea759b4
Bivek Gurung <bivek.tamu@gmail.com>
2019-12-10 14:22:17 +1100

    404 page

--- src/page/Default.js
+++ src/page/Default.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Hero from '../components/Hero';
 
 const Default = () => {
     return (
-        <div>404</div>
+        <main id="main" className="default-page">
+            <Hero page="home" caption="Sorry! Page Not Found" />
+        </main>
     )
 }
 
